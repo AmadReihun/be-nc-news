7ecbeb335f510f57812e2d6d937ecde9ad52bce0
AmadReihun <ahmad.reihani@gmail.com>
2024-01-15 16:13:18 +0000

    do task 3-/api to show endpoints

--- app/controllers/ncTopics.controllers.js
+++ app/controllers/ncTopics.controllers.js
@@ -1,10 +1,21 @@
-const { fetchTopics} = require("../models/ncTopics.models")
+const { fetchTopics, fetchApi} = require("../models/ncTopics.models")
+
 
 exports.getTopics = (req, res, next) => {
   fetchTopics()
   .then((topics) => {
     res.status(200).send({ topics });
-    console.log(topics , '<<<<<< in controller :D ');
+  })
+  .catch((err) => {
+    console.log(err.status);
+    next(err);
+  })
+}
+
+exports.getApi = (req, res, next) => {
+  fetchApi()
+  .then((endpoint) => {
+    res.status(200).send({ endpoint });
   })
   .catch((err) => {
     console.log(err.status);
