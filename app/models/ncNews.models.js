f02f417fa6589d07d7a5f505a1e5b854e623b8f6
AmadReihun <ahmad.reihani@gmail.com>
2024-01-16 17:43:22 +0000

    complete task 6

--- app/models/ncNews.models.js
+++ app/models/ncNews.models.js
@@ -51,3 +51,16 @@ exports.fetchArticles = () => {
 }
 
 
+exports.fetchCommentsbyArticleId = (article_id) => {
+  return db
+    .query(
+      `SELECT * FROM comments WHERE article_id = $1
+      ORDER BY created_at DESC`, [article_id])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({status: 404, msg: "Not Found"})
+      }
+      return rows;
+    });
+}
+
