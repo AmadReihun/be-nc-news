86f710e36017e480387b381307b371a001d2a2bc
AmadReihun <ahmad.reihani@gmail.com>
2024-02-03 10:05:40 +0000

    allow cross origin resource sharing

--- app/app.js
+++ app/app.js
@@ -1,21 +1,38 @@
+const cors = require("cors");
+
 const express = require("express");
-const { getTopics, getApi, getArticleById, getArticles, getCommentsbyArticleId, createCommentByArticleId, updateArticleByArticleId, deleteCommentById, getUsers} = require("./controllers/ncNews.controllers");
-const { psqlErrorHandler, customErrorHandler, serverErrorHandler} = require("./errorHandlers");
+const {
+  getTopics,
+  getApi,
+  getArticleById,
+  getArticles,
+  getCommentsbyArticleId,
+  createCommentByArticleId,
+  updateArticleByArticleId,
+  deleteCommentById,
+  getUsers,
+} = require("./controllers/ncNews.controllers");
+const {
+  psqlErrorHandler,
+  customErrorHandler,
+  serverErrorHandler,
+} = require("./errorHandlers");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get("/api/topics", getTopics);
-app.get("/api", getApi)
-app.get("/api/articles/:article_id", getArticleById)
-app.get("/api/articles", getArticles)
-app.get("/api/articles/:article_id/comments", getCommentsbyArticleId)
+app.get("/api", getApi);
+app.get("/api/articles/:article_id", getArticleById);
+app.get("/api/articles", getArticles);
+app.get("/api/articles/:article_id/comments", getCommentsbyArticleId);
 
-app.post("/api/articles/:article_id/comments", createCommentByArticleId)
+app.post("/api/articles/:article_id/comments", createCommentByArticleId);
 
-app.patch("/api/articles/:article_id",updateArticleByArticleId)
+app.patch("/api/articles/:article_id", updateArticleByArticleId);
 
-app.delete("/api/comments/:comment_id",deleteCommentById)
+app.delete("/api/comments/:comment_id", deleteCommentById);
 
 app.get("/api/users", getUsers);
 
